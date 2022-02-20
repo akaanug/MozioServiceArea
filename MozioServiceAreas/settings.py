0ad4f4daa4502f52ebd1efbfeb8ab580380f0264
akaanug <46591206+akaanug@users.noreply.github.com>
2022-02-20 21:35:05 +0300

    add GDAL to req

--- MozioServiceAreas/settings.py
+++ MozioServiceAreas/settings.py
@@ -34,7 +34,6 @@ REST_FRAMEWORK = {
     ]
 }
 
-SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
